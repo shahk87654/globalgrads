ca7db33667889bc0ff3a8579d1e07e57e2078330
Shahnawaz <shahnawaz03414719656@gmail.com>
2025-07-19 19:32:03 +0500

    first commit

---
+++ api/send-email.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    const { name, email, subject, message } = req.body;
+
+    // Create a transporter object using the Gmail SMTP service
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER, // Your Gmail address
+        pass: process.env.GMAIL_PASS, // Your Gmail password or app-specific password
+      },
+    });
+
+    // Email options
+    const mailOptions = {
+      from: email,
+      to: process.env.GMAIL_USER, // Your Gmail address to receive the emails
+      subject: `${subject}`,
+      text:
+        `Name: ${name}\n\n` +
+        `Email: ${email}\n\n` +
+        `Subject: ${subject}\n\n` +
+        `Message: ${message}\n`,
+    };
+
+    // Send the email
+    try {
+      await transporter.sendMail(mailOptions);
+      res
+        .status(200)
+        .json({ success: true, message: "Email sent successfully!" });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to send email." });
+    }
+  } else {
+    res.status(405).json({ success: false, message: "Method not allowed" });
+  }
+};
